d2d7cdca945738bbc8278b67a0cfbc19f9aece10
dannikaml <dannikaml@hotmail.com>
2023-01-24 20:35:04 -0600

    add p to html for search; fix/add element for API; chore clean up css

--- script.js
+++ script.js
@@ -1,40 +1,35 @@
-<<<<<<< HEAD
 var bookSearchApi = 'https://openlibrary.org/works/OL15626917W.json' ;
 var bookshelfSave = [];
+var booksContainer = document.getElementById('books')
 
 var searchInput = document.querySelector('#search');
 var searchResult = document.querySelector('#bookResults');
 var bookList = document.querySelector('#bookList')
 
 
-fetch(bookSearchApi)
-.then(function (response) {
+
+function generateSearch() {
+    var ulElement = document.createElement("ul")
+
+    fetch(bookSearchApi)
+    .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log('search');
+    console.log('search input');
     console.log(data);
-    // TODO: Loop through the response
-    for (let i = 0; i < 5; i++) {
-      const element = data[i];
-      console.log(data)
-      
-    }
-    // TODO: Console log each issue's URL and each user's login
-  });
-
-
-
-function generateSearch() {
-
+    
+    for (let index = 0; index < data.length; index++) {
+        const element = data[index];
+        console.log(element)
+        var liElement = document.createElement("li")
+        var bookElement = document.createElement("p")
+       // liElement.textContent = `search ${element.search}, url: ${element.url}`
+       // ulElement.append(liElement)
+      }
+      booksContainer.append(ulElement)
+    });
 }
+searchInput.addEventListener('click', generateSearch);
 
 
-=======
-var = 'https://openlibrary.org/api/volumes/brief/isbn/9780525440987.json';
-const bookSearchBox = document.getElementById('search-input')
-const searchList = document.getElementById('')
-function loadBook(searchTerm){
-    const URL = 'https://openlibrary.org/api/volumes/brief/isbn/9780525440987.json';
-}
->>>>>>> 7839f2d4bee502f4bb77e35bd4e401d35361017d
