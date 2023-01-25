3f7f2af0ac0625609d8694e9f2f8cae2e79a6f25
Eibon <ninetimes64@gmail.com>
2023-01-24 18:52:40 -0600

    added brandens JS

---
+++ script.js
@@ -0,0 +1,6 @@
+var = 'https://openlibrary.org/api/volumes/brief/isbn/9780525440987.json';
+const bookSearchBox = document.getElementById('search-input')
+const searchList = document.getElementById('')
+function loadBook(searchTerm){
+    const URL = 'https://openlibrary.org/api/volumes/brief/isbn/9780525440987.json';
+}
