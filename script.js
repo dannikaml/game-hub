4cfa672c1bce1a80686fe49bb39dbfa16143fc85
brendanfrabutt <118411402+brendanfrabutt@users.noreply.github.com>
2023-01-27 11:09:23 -0500

    homepage.html

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
-var searchInput = document.querySelector('#searchInput');
-var bookSearchApi = `https://openlibrary.org/search.json?q=${searchInput}`;
-var bookshelfSave = [];
-var booksContainer = document.getElementById('books')
-
-
-var searchResult = document.querySelector('#bookResults');
-var bookList = document.querySelector('#bookList')
+const bookSearchBox = document.getElementById('searchTextBox');
 
 
 
 
-function generateSearch() {
-    var ulElement = document.createElement("ul")
+let searchInput = document.querySelector("#search-input");
+let url = `https://openlibrary.org/search.json?q=${searchInput}`
 
-    fetch(bookSearchApi)
-    .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('search input');
-    console.log(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var searchInput = document.querySelector('#searchInput');
+//var bookSearchApi = `https://openlibrary.org/search.json?q=${searchInput}`;
+//var bookshelfSave = [];
+//var booksContainer = document.getElementById('books')
+
+
+//var searchResult = document.querySelector('#bookResults');
+//var bookList = document.querySelector('#bookList')
+
+
+
+
+//function generateSearch() {
+    //var ulElement = document.createElement("ul")
+
+    //fetch(bookSearchApi)
+    //.then(function (response) {
+    //return response.json();
+  //})
+  //.then(function (data) {
+    //console.log(searchResult);
+    //console.log(data);
     
-    for (let index = 0; index < data.length; index++) {
-        const element = data[index];
-        console.log(element)
-        var liElement = document.createElement("li")
-        var bookElement = document.createElement("p")
-       liElement.textContent = `search ${element.search}, url: ${element.url}`
-        ulElement.append(liElement)
-      }
-      booksContainer.append(ulElement)
-    });
-}
-searchInput.addEventListener('click', generateSearch);
+    //for (let index = 0; index < data.length; index++) {
+        //const element = data[index];
+       // console.log(element)
+        //var liElement = document.createElement("li")
+        //var bookElement = document.createElement("p")
+       //liElement.textContent = `search ${element.search}, url: ${element.url}`
+        //ulElement.append(liElement)
+      //}
+      //booksContainer.append(ulElement)
+    //});
+//}
+//searchInput.addEventListener('click', generateSearch);
