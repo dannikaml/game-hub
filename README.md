d6d96ddb45431f8296950ce46d7c70e26ca22ee8
dannikaml <dannikaml@hotmail.com>
2023-01-29 13:34:59 -0600

    fix api response, chore relabeled to read like english, add sources to README, fix html-faulty

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Book-Hub
 
+## Description
+***
 
 
+### Usage
+***
+
+### Api Link
+- https://openlibrary.org/dev/docs/api/search
 
 
 
@@ -11,3 +18,23 @@
 
 MDNdocs:
 - https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent
+- https://developer.mozilla.org/en-US/docs/Web/API/Event/target
+
+StackOverflow:
+- https://stackoverflow.com/questions/67193886/display-an-array-of-objects-by-an-api-javascript-for-loop-vs-for-in-loop
+- https://stackoverflow.com/questions/11563638/how-do-i-get-the-value-of-text-input-field-using-javascript
+- https://stackoverflow.com/questions/5700471/set-value-of-input-using-javascript-function
+- https://stackoverflow.com/questions/9855656/how-can-i-submit-a-form-using-javascript
+- https://stackoverflow.com/questions/3547035/getting-html-form-values
+- https://stackoverflow.com/questions/54905225/
+- https://stackoverflow.com/questions/54905225/
+- https://javascript-for-loop-to-retrieve-and-print-a-certain-amount-of-api-data
+
+Google:
+- https://www.google.com/search?q=set+inner+text+of+div+javascript&rlz=1C1CHBF_enUS1037US1037&oq=set+inner+text+of+&aqs=chrome.0.0i512j69i57j0i512j0i22i30l5j0i390l2.4257j0j7&sourceid=chrome&ie=UTF-8
+
+w3schools:
+- https://www.w3schools.com/tags/att_button_type.asp
+- https://www.w3schools.com/js/js_loop_for.asp
+
+
--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 var searchBtnEl = document.querySelector('#searchBtn');
 var searchboxEl = document.querySelector("#searchbox");
+var form = document.getElementById('form');
+console.log(searchboxEl);
 //var searchValue = searchbox.innerText.trim()
 //var bookSearchApi = `https://openlibrary.org/search.json?q=${searchValue}`;
 var bookshelfSave = [];
-var booksContainer = document.querySelector("#books");
+var booksContainer = document.querySelector("#books-container");
 var bookSearchTerm = document.querySelector('#search-term');
 
 
 
-var searchResult = document.querySelector('#bookResults');
-var bookList = document.querySelector('#bookList');
-console.log(searchboxEl)
+//var searchResult = document.querySelector('#bookResults');
+//var bookList = document.querySelector('#bookList');
+
 
 var searchHandler = function (event) {
-  event.preventDefault();
+  event.preventDefault(); 
+  console.log('event in searchHandler: ', event);
 
   var searchInput = searchboxEl.value.trim();
 
@@ -27,58 +30,68 @@ var searchHandler = function (event) {
   }
 };
 
-var generateSearch = function (docs) {
-    var searchValue = searchboxEl.value.trim()
-    var bookSearchURL = `https://openlibrary.org/search.json?q=${searchValue}`;
-    console.log(searchValue)
+var formSubmitHandler = function (event) {
+  // Don't refresh the page
+  event.preventDefault();
 
-   fetch(bookSearchURL)
-   .then(function (response) {
-    if (response.ok) {
-      console.log(response);
-      response.json().then(function (data) {
-        console.log(data);
-        console.log(docs);
-        displaySearch(data, docs);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  })
-  .catch(function (error) {
-    alert('Unable to connect to Open Library');
-  });
-};
+  console.log('submit event: ', event);
+  console.log(event.target[0].value);
 
-var displaySearch = function (docs, searchTerm) {
-  if (docs.length === 0) {
+  var searchValue = event.target[0].value;
+  var bookSearchURL = `https://openlibrary.org/search.json?q=${searchValue}`;
+  console.log('generateSearch searchValue: ', searchValue);
+
+  // update UI to show the search term
+  bookSearchTerm.innerText = event.target[0].value;
+
+  // call the api
+ fetch(bookSearchURL)
+ .then(function (apiDataResponse) {
+  console.log('response in generateSearch: ', apiDataResponse);
+  if (apiDataResponse.ok) {
+    console.log('apiDataResponse is okay!');
+    apiDataResponse.json().then(function (apiDataResponse) {
+      console.log('apiDataResponse: ', apiDataResponse);
+      console.log('apiDataResponse event: ', event);
+      displaySearch(apiDataResponse, event);
+    });
+  } else {
+    console.log('response failed :(');
+    alert('Error: ' + response.statusText);
+  }
+})
+.catch(function (error) {
+  alert('Unable to connect to Open Library: ' + error);
+});
+}
+
+var displaySearch = function (apiDataResponse, event) {
+  console.log('apiDataResponse.length: ', apiDataResponse.docs.length);
+  if (apiDataResponse.docs.length === 0) {
     booksContainer.textContent = 'Search not found.';
-    return;
+  } else {
+     apiDataResponse.docs.map(function(book) {
+      console.log(book);
+     })
+
+    console.log('i am in the else statement');
   }
+}
 
-  bookSearchTerm.textContent = searchTerm;
+//    // something is wrong here
+//     for (var i = 0; i < apiDataResponse.docs.length; i++) {
+//       var bookTitle = apiDataResponse[i] + '/' + apiDataResponse[i];
+//       // not getting to this console log
+//       console.log('bookTitle', bookTitle);
 
-  for (var i = 0; i < docs.length; i++) {
-    var bookTitle = docs[i].title + '/' + docs[i].name;
 
-    var bookEl = document.createElement('div');
-    bookEl.classList = 'list-item flex-row justify-space-between align-center';
+//       document.getElementById("books-container").innerHTML = 
+    
 
-    var titleEl = document.createElement('span');
-    titleEl.textContent = bookTitle;
-
-    bookEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
 
     
-    booksContainer.appendChild(bookEl);
-  }
-};
-
-    
-searchBtnEl.addEventListener('click', generateSearch);
+// // searchBtnEl.addEventListener('click', generateSearch);
+form.addEventListener("submit", formSubmitHandler);
 
 
 
