7736fbcf42b4ef13ee5f169206a17dee6ff400c6
dannikaml <dannikaml@hotmail.com>
2023-01-30 09:45:11 -0600

    fix search field display in js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 var searchBtnEl = document.querySelector('#searchBtn');
 var searchboxEl = document.querySelector("#searchbox");
+    console.log(searchboxEl);
 var form = document.getElementById('form');
-console.log(searchboxEl);
-//var searchValue = searchbox.innerText.trim()
-//var bookSearchApi = `https://openlibrary.org/search.json?q=${searchValue}`;
-var bookshelfSave = [];
+
 var booksContainer = document.querySelector("#books-container");
 var bookSearchTerm = document.querySelector('#search-term');
-
+var bookshelfSave = [];
 
 
 //var searchResult = document.querySelector('#bookResults');
@@ -66,12 +64,12 @@ var formSubmitHandler = function (event) {
 }
 
 var displaySearch = function (apiDataResponse, event) {
-  console.log('apiDataResponse.length: ', apiDataResponse.docs.length);
+  console.log('apiDataResponse.length: ---WHERE AM I? ', apiDataResponse.docs.length);
   if (apiDataResponse.docs.length === 0) {
     booksContainer.textContent = 'Search not found.';
   } else {
      apiDataResponse.docs.map(function(book) {
-      //console.log(book);
+      console.log(book);
      })
 
     console.log('i am in the else statement');
@@ -80,41 +78,20 @@ var displaySearch = function (apiDataResponse, event) {
    // something is wrong here
    for (let index = 0; index < apiDataResponse.docs.length; index++) {
     var ulElement = document.createElement('div');
-    ulElement.classList = 'list-group flex-row justify-space-between align-center';
-    //console.log('WHAT IS HAPPENING?');
+    ulElement.classList = 'list-group';
+    console.log('WHAT IS HAPPENING?');
 
         const element = apiDataResponse.docs[index];
-       // console.log(element, "CAN YOU SEE ME?");
+        console.log(element, "CAN YOU SEE ME?");
 
-        var liElement = document.createElement("div");
-        liElement.innerHTML = `Book Title: ${element.title}, Author Work Key: ${element.key}`;
+        var liElement = document.createElement('span');
+        liElement.innerHTML = `Book Title: ${element.title}, Subtitle: ${element.subtitle}, Date Published: ${element.publish_date}, Author: ${element.author_name}, Languages: ${element.language}, Publisher: ${element.publisher}, Number of Pages: ${element.number_of_pages_median}`;
         ulElement.append(liElement)
-        //console.log("YOU BETTER BE THERE")
+        console.log("YOU BETTER BE THERE")
       }
       booksContainer.append(ulElement)
+      console.log("am i working?")
     
-    }
-    
-    
+ }
 // searchBtnEl.addEventListener('click', generateSearch);
 form.addEventListener("submit", formSubmitHandler);
-
-    
-
-
-
-
-  //  fetch('https://openlibrary.org/search.json?q=${searchValue}') 
-    //  .then(response => response.json()) 
-    // .then(data => { const selectedData = data.filter(item => item.property === 'value'); 
-    // console.log(selectedData); }); 
-    
-    // for (let index = 0; index < data.length; index++) {
-    //     const element = data[index];
-    //     console.log(element)
-    //     var liElement = document.createElement("li")
-    //     var bookElement = document.createElement("p")
-    //     liElement.textContent = `search ${element.search}, url: ${element.url}`
-    //     ulElement.append(liElement)
-    //   }
-    //   booksContainer.append(ulElement)
