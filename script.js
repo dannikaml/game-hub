dfff5c451b6794b650e031de9c9211b378ca749d
John Klink <john.r.klink92@gmail.com>
2023-01-30 19:23:33 -0600

    added local storage

--- script.js
+++ script.js
@@ -42,20 +42,56 @@ var formSubmitHandler = function (event) {
   // update UI to show the search term
   bookSearchTerm.innerText = event.target[0].value;
 
-    fetch(bookSearchURL)
-    .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(searchValue);
-    console.log(data);
+  // call the api
+ fetch(bookSearchURL)
+ .then(function (apiDataResponse) {
+  console.log('response in generateSearch: ', apiDataResponse);
+  if (apiDataResponse.ok) {
+    console.log('apiDataResponse is okay!');
+    apiDataResponse.json().then(function (apiDataResponse) {
+      console.log('apiDataResponse: ', apiDataResponse);
+      console.log('apiDataResponse event: ', event);
+      displaySearch(apiDataResponse, event);
+    });
+  } else {
+    console.log('response failed :(');
+    alert('Error: ' + response.statusText);
+  }
+})
+.catch(function (error) {
+  alert('Unable to connect to Open Library: ' + error);
+});
+}
+
+//local sotrage
+localStorage.setItem('bookKey', searchValue)
+localStorage.getItem('bookKey')
+
+var displaySearch = function (apiDataResponse, event) {
+  console.log('apiDataResponse.length: ---WHERE AM I? ', apiDataResponse.length);
+
+  var ulElement = document.createElement('div');
+    ulElement.classList = 'list-group';
+    console.log('WHAT IS HAPPENING?');
+  
+  if (apiDataResponse.docs.length === 0) {
+    booksContainer.textContent = 'Search not found.';
+  } else {
+     apiDataResponse.docs.map(function(book) {
+      //console.log(book);
+     })
+
+    console.log('i am in the else statement');
+  }
+
+   // something is wrong here
+   for (let index = 0; index < apiDataResponse.docs.length; index++) {
+       const element = apiDataResponse.docs[index];
+        console.log(element, "CAN YOU SEE ME?");
     
-    for (let index = 0; index < data.length; index++) {
-        const element = data[index];
-        console.log(element)
-        var liElement = document.createElement("li")
-        var bookElement = document.createElement("p")
-        liElement.textContent = `search ${element.search}, url: ${element.url}`
+
+        var liElement = document.createElement('span');
+        liElement.innerHTML = `Book Title: ${element.title}, Subtitle: ${element.subtitle}, Date Published: ${element.publish_date}, Author: ${element.author_name}, Languages: ${element.language}, Publisher: ${element.publisher}, Number of Pages: ${element.number_of_pages_median}`;
         ulElement.append(liElement)
         console.log(liElement, "YOU BETTER BE THERE")
       
@@ -64,4 +100,5 @@ var formSubmitHandler = function (event) {
     
  }
 }
-searchBtnEl.addEventListener('click', generateSearch);
+// searchBtnEl.addEventListener('click', generateSearch);
+form.addEventListener("submit", formSubmitHandler);
